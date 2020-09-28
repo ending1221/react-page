cf015f85d7006bc7e42c05c98957307bf40b9506
Yau <ending1221@gmail.com>
2020-09-29 00:05:21 +0800

    Updates

---
+++ precache-manifest.1d5a7159e6583ff834b8450ca76725ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f631a3e43ffd0cf9ff0639e3538f2132",
+    "url": "/react-page/index.html"
+  },
+  {
+    "revision": "43325061e0dbb3094dad",
+    "url": "/react-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5bd93647851e2b500984",
+    "url": "/react-page/static/js/2.e37b2f0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-page/static/js/2.e37b2f0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43325061e0dbb3094dad",
+    "url": "/react-page/static/js/main.790368aa.chunk.js"
+  },
+  {
+    "revision": "eb18cec803fae7134401",
+    "url": "/react-page/static/js/runtime-main.f8eddd1f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
