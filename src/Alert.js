063b45c2104dd92a5fe719011a139f1217452ea5
Yau <ending1221@gmail.com>
2020-10-11 21:02:28 +0800

    新增 Alert

---
+++ src/Alert.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+import style from "./css/Alert.module.css";
+
+class Alert extends Component {
+    render() {
+        const { payEvent, pay, deleteOrder, deleteIndex, canel } = this.props;
+        console.log("canel", canel);
+        const payHtml = (
+            <div className={style.outbox}>
+                <div className={style.box}>
+                    <p>您已結帳，謝謝！</p>
+                    <div className={style.btn} onClick={payEvent}>
+                        關閉
+                    </div>
+                </div>
+            </div>
+        );
+        const deleteHtml = (
+            <div className={canel ? style.outbox : style.displayNone}>
+                <div className={style.box}>
+                    <span>是否刪除此商品?</span>
+                    <div>
+                        <button
+                            className={style.btn + " " + style.small}
+                            onClick={() => deleteOrder(deleteIndex)}
+                        >
+                            是
+                        </button>
+                        <button
+                            className={style.btn + " " + style.small}
+                            onClick={() => setAlert(false)}
+                        >
+                            否
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+        return pay ? payHtml : deleteHtml;
+    }
+}
+
+export default Alert;
